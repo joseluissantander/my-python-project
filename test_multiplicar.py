9745e8b4ce93945781b78542d5810faf655d6a01
joseluissantander <131145782+joseluissantander@users.noreply.github.com>
2023-05-08 16:17:12 +0200

    Update test_multiplicar.py

--- test_multiplicar.py
+++ test_multiplicar.py
@@ -1,5 +1,5 @@
 from my_python_code import multiplicar
 def test_multiplicar():
- assert multiplicar(2, 3) == 6
+ assert multiplicar(2, 3) == 7
  assert multiplicar(0, 0) == 0
  assert multiplicar(-1, 1) == -1
